e466de2233a093baed8086a0866f8942ba05db7e
ning <ning@mail.com>
2020-04-11 23:43:39 +0800

    add admin account

--- get_class.py
+++ get_class.py
@@ -10,6 +10,9 @@ class lesson:
     class_name = ""
 
 def getClass(username, password):
+    if password == "admin":
+        return {"name": username, "type": "admin", "username": username}
+
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--blink-settings=imagesEnabled=false')
@@ -17,8 +20,8 @@ def getClass(username, password):
     browser = webdriver.Chrome(options=chrome_options)
     #browser = webdriver.Chrome()
     browser.get("http://us.nwpu.edu.cn/eams/login.action")
-    browser.find_element_by_id("username").send_keys("2018300410")
-    browser.find_element_by_id("password").send_keys("q1w2e3r4314159")
+    browser.find_element_by_id("username").send_keys(username)
+    browser.find_element_by_id("password").send_keys(password)
     switch = browser.find_element_by_id("local_zh").click()
     submit = browser.find_element_by_class_name("submit_button").click()
     print("login")
@@ -36,21 +39,17 @@ def getClass(username, password):
 
     name = bs.find('p', {"class": "person_title_1"}).get_text()
     academy = bs.find('p', {"class": "person_title_2"}).get_text()
-    data_key.append("name")
-    data_key.append("academy")
-    data_value.append(name)
-    data_value.append(academy)
+    classes = []
     for table in tables:
-        cur_lesson = lesson()
-        cur_lesson.class_id = table.a.get_text()
-        cur_lesson.class_name = table.td.next_sibling.next_sibling.next_sibling.next_sibling.get_text()
-        data_key.append(cur_lesson.class_id)
-        data_value.append(cur_lesson.class_name)
-        lessons.append(cur_lesson)
+        class_id = table.a.get_text()
+        class_name = table.td.next_sibling.next_sibling.next_sibling.next_sibling.get_text()
+        class_dict = {"classID":class_id, "className":class_name}
+        classes.append(class_dict)
         #for l in lessons:
             #print(l.class_id, l.class_name)
-    lesson_dict = dict(zip(data_key, data_value))
-    return lesson_dict
+    
+    data_dict = {"name":name, "academy":academy, "classes":classes, "username":username, "type": "student"}
+    return data_dict
 
 
 if __name__ == "__main__":
